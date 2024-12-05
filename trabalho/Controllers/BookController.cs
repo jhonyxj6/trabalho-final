a0e439a691a5188cb368d86e9edf7413bfb617fa
Jhony Gustavo <jhonygustavocd@gmail.com>
2024-12-05 19:12:21 -0300

    adjust

--- trabalho/Controllers/BookController.cs
+++ trabalho/Controllers/BookController.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using trabalho.Models;
@@ -11,32 +12,23 @@ namespace trabalho.Controllers
     {
         private static List<ModeloLivros> livros = new List<ModeloLivros>()
         {
-               new ModeloLivros{Id=1, autor ="Dom Casmurro",Titulo="Machado de Assis", quantidade=2},
-                new ModeloLivros{Id=2,autor ="Memórias Póstumas de Brás Cubas",Titulo="Machado de Assis", quantidade=2},
-               new ModeloLivros{Id=3,autor ="João Guimarães Rosa",Titulo="Grande Sertão: Veredas", quantidade=4},
-               new ModeloLivros{Id=4,autor ="Aluísio Azevedo",Titulo="O Cortiço", quantidade=4},
-               new ModeloLivros{Id=5,autor ="José de Alencar",Titulo="Iracema", quantidade=1},
-               new ModeloLivros{Id=6,autor ="Mário de Andrade",Titulo="Macunaíma", quantidade=11},
-               new ModeloLivros{Id=7,autor ="Jorge Amado",Titulo="Capitães da Areia", quantidade=2},
-               new ModeloLivros{Id=8,autor ="Graciliano Ramos",Titulo="Vidas Secas", quantidade=9},
-               new ModeloLivros{Id=9,autor ="Joaquim Manuel de Macedo",Titulo="A Moreninha", quantidade=2},
-               new ModeloLivros{Id=10,autor ="Erico Verissimo",Titulo="O Tempo e o Vento", quantidade=1},
-               new ModeloLivros{Id=11,autor ="Clarice Lispector",Titulo="A Hora da Estrela", quantidade=1},
-               new ModeloLivros{Id=12,autor ="Rachel de Queiroz",Titulo="O Quinze", quantidade=1},
-               new ModeloLivros{Id=13,autor ="José Lins do Rego",Titulo="Menino do Engenho", quantidade=5},
-               new ModeloLivros{Id=14,autor ="João Guimarães Rosa",Titulo="Sagarana", quantidade=3},
-               new ModeloLivros{Id=15,autor ="José Lins do Rego",Titulo="Fogo Morto", quantidade=1},
-
-
-
-
+               new ModeloLivros{Id=1, autor ="Dom Casmurro",Titulo="Machado de Assis", quantidade=2, ano = 1899},
+                new ModeloLivros{Id=2,autor ="Memórias Póstumas de Brás Cubas",Titulo="Machado de Assis", quantidade=2, ano= 1881},
+               new ModeloLivros{Id=3,autor ="João Guimarães Rosa",Titulo="Grande Sertão: Veredas", quantidade=4,ano= 1956},
+               new ModeloLivros{Id=4,autor ="Aluísio Azevedo",Titulo="O Cortiço", quantidade=4, ano= 1890},
+               new ModeloLivros{Id=5,autor ="José de Alencar",Titulo="Iracema", quantidade=1, ano =1865},
+               new ModeloLivros{Id=6,autor ="Mário de Andrade",Titulo="Macunaíma", quantidade=11, ano=1928},
+               new ModeloLivros{Id=7,autor ="Jorge Amado",Titulo="Capitães da Areia", quantidade=2, ano = 1937},
+               new ModeloLivros{Id=8,autor ="Graciliano Ramos",Titulo="Vidas Secas", quantidade=9, ano=1938},
+               new ModeloLivros{Id=9,autor ="Joaquim Manuel de Macedo",Titulo="A Moreninha", quantidade=2, ano=1844},
+               new ModeloLivros{Id=10,autor ="Erico Verissimo",Titulo="O Tempo e o Vento", quantidade=1, ano =1949},
+               new ModeloLivros{Id=11,autor ="Clarice Lispector",Titulo="A Hora da Estrela", quantidade=1, ano=1977},
+               new ModeloLivros{Id=12,autor ="Rachel de Queiroz",Titulo="O Quinze", quantidade=1, ano=1930},
+               new ModeloLivros{Id=13,autor ="José Lins do Rego",Titulo="Menino do Engenho", quantidade=5, ano= 1932},
+               new ModeloLivros{Id=14,autor ="João Guimarães Rosa",Titulo="Sagarana", quantidade=3, ano =1946},
+               new ModeloLivros{Id=15,autor ="José Lins do Rego",Titulo="Fogo Morto", quantidade=1, ano = 1943},
 
         };
-
-
-
-
-
         [HttpPost]
         public ActionResult<List<ModeloLivros>> AdicionarLivro(ModeloLivros novoLivros)
         {
@@ -51,13 +43,90 @@ namespace trabalho.Controllers
         public ActionResult<List<ModeloLivros>> ListaLivros()
         {
             return Ok(livros);
+        }
+
+
+        [HttpPost("alugar/{Titulo}")]
+        public ActionResult AlugarPorTitulo(string Titulo)
+        {
+
+            var encontarlivro = livros.Find(l => l.Titulo == Titulo);
+
+            if (encontarlivro == null)
+            {
+                return NotFound("livro não encontrado");
+            }
+            if (encontarlivro.alugar == true)
+            {
+                return NotFound("Este livro já está alugado");
+
+            }
+            if (encontarlivro.alugar = true) ;
+
+            {
+                return Ok($"{encontarlivro.Titulo} foi alugado com sucesso!!!! ");
+
+            }
+
+
+
+
 
         }
 
-       
-    }   
+        [HttpPost("devolver/{titulo}")]
+        public ActionResult DevolverLivros(string titulo)
+        {
+            var devolverlivri = livros.Find(l => l.Titulo == titulo);
+
+            if (devolverlivri == null)
+            {
+                return NotFound("não econtado");
+            }
+
+            if (devolverlivri.alugar == false)
+            {
+                return NotFound("livro não alugadod");
+            }
+            devolverlivri.alugar = false;
+
+            devolverlivri.quantidade++;
+
+            return Ok($"Livro '{devolverlivri.Titulo}' devolvido com sucesso.");
+        }
+
+
+        [HttpGet("alugados")]
+        public ActionResult<List<ModeloLivros>> LivrosAlugados()
+        {
+            var encontraalugado = livros.FindAll(l => l.alugar);
+
+
+            if (encontraalugado.Count == 0)
+            {
+                return NotFound("Não há livros alugados agora ");
+            }
+            return Ok(encontraalugado);
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
 
 
 }
-
-
--- trabalho/Models/Livros.cs
+++ trabalho/Models/Livros.cs
@@ -1,11 +1,25 @@
-﻿namespace trabalho.Models
+﻿using System.Data;
+using System.IO.Pipes;
+using System.Text.Json.Serialization.Metadata;
+
+namespace trabalho.Models
 {
     public class ModeloLivros
     {
-        public  int Id { get; set; }    
+        public int Id { get; set; }
         public string Titulo { get; set; }
-        public string autor {  get; set; }
+        public string autor { get; set; }
 
-        public int quantidade   { get; set; }
+        public int quantidade { get; set; }
+
+        public bool alugar { get; set; }
+
+        public int ano { get; set; }
+
+
     }
 }
+
+
+
+
